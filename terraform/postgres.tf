0ae1feb65e191f0843c3431f41b26b8767218818
Herwig Bogaert <herwig.bogaert@meemoo.be>
2023-05-25 15:21:09 +0200

    remove mongo db and add postgres

---
+++ terraform/postgres.tf
@@ -0,0 +1,87 @@
+variable "password_db_postgres-int_admin" {
+  type = string
+}
+
+variable "password_db_postgres-qas_admin" {
+  type = string
+}
+
+variable "password_db_postgres-prd_admin" {
+type = string
+}
+
+resource "ibm_database" "pg-give-services-int" {
+  resource_group_id            = ibm_resource_group.qas.id
+  name                         = "pg-give-services-int"
+  service                      = "databases-for-postgresql"
+  plan                         = "standard"
+  location                     =  var.location
+  version                      = "14"
+  adminpassword                = var.password_db_postgres-int_admin
+
+  group {
+    group_id = "member"
+    memory {
+      allocation_mb = 1024
+    }
+    disk {
+      allocation_mb = 5120
+    }
+    cpu {
+      allocation_count = 0
+    }
+  }
+
+  service_endpoints            = "private"
+}
+
+resource "ibm_database" "pg-give-services-qas" {
+  resource_group_id            = ibm_resource_group.qas.id
+  name                         = "pg-give-services-qas"
+  service                      = "databases-for-postgresql"
+  plan                         = "standard"
+  location                     =  var.location
+  version                      = "14"
+  adminpassword                = var.password_db_postgres-qas_admin
+
+  group {
+    group_id = "member"
+    memory {
+      allocation_mb = 1024
+    }
+    disk {
+      allocation_mb = 5120
+    }
+    cpu {
+      allocation_count = 0
+    }
+  }
+
+  service_endpoints            = "private"
+}
+
+resource "ibm_database" "pg-give-services-prd" {
+  resource_group_id            = ibm_resource_group.prd.id
+  name                         = "pg-give-services-prd"
+  service                      = "databases-for-postgresql"
+  plan                         = "standard"
+  location                     =  var.location
+  version                      = "14"
+  adminpassword                = var.password_db_postgres-prd_admin
+
+  group {
+    group_id = "member"
+    memory {
+      allocation_mb = 2048
+    }
+    disk {
+      allocation_mb = 10240
+    }
+    cpu {
+      allocation_count = 0
+    }
+  }
+
+  service_endpoints            = "private"
+}
+
